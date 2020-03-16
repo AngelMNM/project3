ee52dc0e5406ee157c649a809ad35f725ba1df8d
Angel Negron <negron.a@ufl.edu>
2020-03-15 20:54:03 -0400

    Images, nav css and play tests done

---
+++ scripts/javascript.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
+
+});
+
+
+/* Smoosthscroll script...guess it needs to be in the html
+
+ var scroll = new SmoothScroll('a[href*="#"]');
+
+ */
